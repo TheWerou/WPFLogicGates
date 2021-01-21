7151f030a8873ab792125a55aa7f3e7d2ca2cfda
Wojciech Buńka <wojtek.bunka@gmail.com>
2021-01-21 17:48:17 +0100

    added some code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using BramkiLogiczne.ProgramLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BramkiLogiczne
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LogicGates bramki { get; set; }
+
+        public MainWindow()
+        {
+            bramki =  new LogicGates();
+
+            InitializeComponent();
+        }
+
+        private void AndBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if(bramki.AndGate(AndBut1.IsChecked, AndBut2.IsChecked))
+            {
+                AndWynik.IsChecked = true;
+            }
+            else
+            {
+                AndWynik.IsChecked = false;
+            }
+        }
+
+        private void OrBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (bramki.OrGate(OrBut1.IsChecked, OrBut2.IsChecked))
+            {
+                OrWynik.IsChecked = true;
+            }
+            else
+            {
+                OrWynik.IsChecked = false;
+            }
+        }
+
+        private void NotBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (bramki.NotGate(NotBut1.IsChecked))
+            {
+                NotWynik.IsChecked = true;
+            }
+            else
+            {
+                NotWynik.IsChecked = false;
+            }
+        }
+
+        private void XorBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (bramki.XorGate(XorBut1.IsChecked, XorBut2.IsChecked))
+            {
+                XorWynik.IsChecked = true;
+            }
+            else
+            {
+                XorWynik.IsChecked = false;
+            }
+        }
+
+        private void NandBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (bramki.NandGate(NandBut1.IsChecked, NandBut2.IsChecked))
+            {
+                NandWynik.IsChecked = true;
+            }
+            else
+            {
+                NandWynik.IsChecked = false;
+            }
+        }
+
+        private void NorBut1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (bramki.NorGate(NorBut1.IsChecked, NorBut2.IsChecked))
+            {
+                NorWynik.IsChecked = true;
+            }
+            else
+            {
+                NorWynik.IsChecked = false;
+            }
+        }
+
+    }
+}
---
+++ ProgramLogic/LogicGates.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BramkiLogiczne.ProgramLogic
+{
+    public class LogicGates
+    {
+
+        public bool AndGate(bool? Button1, bool? Button2)
+        {
+            if(Button1 == Button2 && Button1 == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool OrGate(bool? Button1, bool? Button2)
+        {
+
+            if(Button1 != null && Button2 != null)
+            {
+                
+                if(Button1 == true)
+                {
+                    return true;
+                }
+                else if(Button2 == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool NotGate(bool? Button1)
+        {
+            if(Button1 != null)
+            {
+                if(Button1 == true)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool XorGate(bool? Button1, bool? Button2)
+        {
+            if (Button1 != null && Button2 != null)
+            {
+                if ((Button1 == true && Button2 == false) || (Button1 == false && Button2 == true))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+               
+            return false;
+        }
+
+        public bool NandGate(bool? Button1, bool? Button2)
+        {
+            return !AndGate(Button1, Button2);
+        }
+
+        public bool NorGate(bool? Button1, bool? Button2)
+        {
+            return !OrGate(Button1, Button2);
+        }
+    }
+}
